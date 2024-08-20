35b6b71146600a8151910e6e4a34f3d05615a126
Esteban Acevedo <tucorreo@correo>
2024-08-19 22:06:09 -0300

    feat(clientes): listado de clientes

--- proyecto/clientes/urls.py
+++ proyecto/clientes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("pais/list", views.pais_list)
+    path("pais/list", views.pais_list),
+    path("cliente/list", views.cliente_list),
 ]
--- proyecto/clientes/views.py
+++ proyecto/clientes/views.py
@@ -6,3 +6,9 @@ def pais_list(request):
     paises = Pais.objects.all()
     contexto = {'paises': paises}
     return render(request, 'clientes/pais_list.html', contexto)
+
+
+def cliente_list(request):
+    clientes = Cliente.objects.all()
+    contexto = {'clientes': clientes}
+    return render(request, 'clientes/cliente_list.html', contexto)
