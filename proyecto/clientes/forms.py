82df075d6c1e268827dfba4ad3aeafd591405bb2
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2024-08-26 22:17:21 -0300

    sync

---
+++ proyecto/clientes/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Pais
+
+
+class PaisForm(forms.ModelForm):
+    class Meta:
+        model = Pais
+        fields = '__all__'
+
+    def clean_nombre(self):
+        nombre: str = self.cleaned_data.get('nombre', '')
+
+        # Validar que solo contenga letras
+        if not nombre.isalpha():
+            raise ValidationError('El nombre sólo puede contener letras')
+
+        # Validar longitud de nombre
+        if len(nombre) < 3 or len(nombre) > 50:
+            raise ValidationError(
+                'El nombre debe tener una longitud mínima de 3 letras o máxima de 50'
+            )
+        return nombre
--- proyecto/clientes/views.py
+++ proyecto/clientes/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import PaisForm
 from .models import Cliente, Pais
 
 
@@ -17,3 +18,17 @@ def cliente_list(request):
     clientes = Cliente.objects.all()
     contexto = {'clientes': clientes}
     return render(request, 'clientes/cliente_list.html', contexto)
+
+
+def pais_create(request):
+    if request.method == 'GET':
+        form = PaisForm()
+
+    if request.method == 'POST':
+        form = PaisForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            form.save()
+            return redirect('clientes:pais_list')
+
+    return render(request, 'clientes/pais_create.html', {'form': form})
